a19525b804dc5a936bb6073f5a4b1c7ed1813763
ErolB <eroljb@hotmail.com>
2019-07-21 20:13:39 -0400

    trying to deploy

--- genome/settings.py
+++ genome/settings.py
@@ -129,8 +129,8 @@ CACHES = {
     }
 }
 
-BROKER_URL = os.environ.get('REDIS_URL')
-CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
