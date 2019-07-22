0a05433cbaab39695df7e1c403eccda0792d3a70
ErolB <eroljb@hotmail.com>
2019-07-21 20:17:18 -0400

    fixing something?

--- genome/settings.py
+++ genome/settings.py
@@ -128,9 +128,10 @@ CACHES = {
          "LOCATION": os.environ.get('REDIS_URL'),
     }
 }
-
+'''
 BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+'''
