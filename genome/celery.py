62e86471213b3f97d3f25fd8d22779cf998a2b1c
ErolB <eroljb@hotmail.com>
2019-08-27 22:03:36 -0400

    trying something

--- genome/celery.py
+++ genome/celery.py
@@ -6,10 +6,11 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'genome.settings')
 #os.environ.setdefault('REDIS_URL', 'redis://localhost:6379/0')
-app = Celery('genome_app')
+redis_url = os.environ['REDIS_URL']
+app = Celery('genome_app', broker=redis_url, backend=redis_url)
 
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+#app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+#                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
