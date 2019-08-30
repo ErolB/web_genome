ed3f84da14176ad4c35e9bec806ce1053d17ef24
ErolB <eroljb@hotmail.com>
2019-08-29 23:45:49 -0400

    more hardcoding

--- genome/celery.py
+++ genome/celery.py
@@ -6,9 +6,10 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'genome.settings')
 #os.environ.setdefault('REDIS_URL', 'redis://localhost:6379/0')
-os.environ.setdefault('REDIS_URL', 'redis://h:pe133a585ca010fe846e05901e80acc9dcd0a7c23a7dc80c4ab22ae8cdaed7c1c@ec2-3-221-165-119.compute-1.amazonaws.com:23889')
+broker_url = 'redis://h:pe133a585ca010fe846e05901e80acc9dcd0a7c23a7dc80c4ab22ae8cdaed7c1c@ec2-3-221-165-119.compute-1.amazonaws.com:23889'
+os.environ.setdefault('REDIS_URL', broker_url)
 print('Redis URL: %s' % os.environ['REDIS_URL'])
-app = Celery('genome_app', broker=os.environ['REDIS_URL'], backend=os.environ['REDIS_URL'])
+app = Celery('genome_app', broker=broker_url, backend=broker_url)
 
 app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
                 CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
