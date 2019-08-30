8f56ee4797c14bbc5a5df034aa9191efc5f3fb07
ErolB <eroljb@hotmail.com>
2019-08-30 00:10:27 -0400

    Revert "hardcoding (desparately)"
    This reverts commit 99d5267ac94a2f518cda69e8ce9eabe452abc9bc.

--- genome/celery.py
+++ genome/celery.py
@@ -4,15 +4,14 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-redis_url = 'redis://h:pe133a585ca010fe846e05901e80acc9dcd0a7c23a7dc80c4ab22ae8cdaed7c1c@ec2-3-221-165-119.compute-1.amazonaws.com:23889'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'genome.settings')
 #os.environ.setdefault('REDIS_URL', 'redis://localhost:6379/0')
-os.environ.setdefault('REDIS_URL', redis_url)
+os.environ.setdefault('REDIS_URL', 'redis://h:pe133a585ca010fe846e05901e80acc9dcd0a7c23a7dc80c4ab22ae8cdaed7c1c@ec2-3-221-165-119.compute-1.amazonaws.com:23889')
 print('Redis URL: %s' % os.environ['REDIS_URL'])
-app = Celery('genome_app', broker=redis_url, backend=redis_url)
+app = Celery('genome_app', broker=os.environ['REDIS_URL'], backend=os.environ['REDIS_URL'])
 
-app.conf.update(BROKER_URL=redis_url,
-                CELERY_RESULT_BACKEND=redis_url)
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
