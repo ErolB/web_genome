b0d7143609192a955e26211d6fa8b3783e796a37
ErolB <eroljb@hotmail.com>
2019-09-03 21:44:02 -0400

    configuration for Heroku

--- genome/settings.py
+++ genome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,5 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_IMPORTS = ("finder.tasks",)
 
+# Heroku setup
+django_heroku.settings(locals())
