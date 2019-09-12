6e67f1e789b44815a2bf796c4b70d0751cab8fd6
ErolB <eroljb@hotmail.com>
2019-09-11 23:08:35 -0400

    fixing something stupid

--- finder/tasks.py
+++ finder/tasks.py
@@ -12,10 +12,6 @@ def create_genomes(approved_genomes):
         print(genome)
         genome_entry = GenomeModel(organism=genome.organism, genome_id=genome.id)
         genome_entry.save()
-        for gene in genome.genes.values():
-            log_file = open('/home/erol/error_tests.txt', 'w')
-            log_file.write(gene.name)
-            log_file.write(gene.patric_id)
-            log_file.close()
+        for gene in genome.genes.values()
             gene_entry = GeneModel(name=gene.name, sequence=gene.sequence, in_genome=genome_entry, patric_id=gene.patric_id)
             gene_entry.save()
