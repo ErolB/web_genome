e36481d5ad2a57c5c8676b51e513cd847db5a80c
ErolB <eroljb@hotmail.com>
2019-09-11 22:56:05 -0400

    accommodates longer protein and motif sequences

--- finder/models.py
+++ finder/models.py
@@ -10,7 +10,7 @@ class GenomeModel(models.Model):
 # protein-coding gene
 class GeneModel(models.Model):
     name = models.CharField(max_length=200)
-    sequence = models.CharField(max_length=1000)
+    sequence = models.CharField(max_length=10000)
     patric_id = models.CharField(max_length=300)
     fig_id = models.CharField(max_length=400)
     in_genome = models.ForeignKey(GenomeModel, on_delete=models.CASCADE)
@@ -19,6 +19,6 @@ class GeneModel(models.Model):
 # search method
 class MotifSearchModel(models.Model):
     gene_name = models.CharField(max_length=100)
-    motif_text = models.CharField(max_length=100)
+    motif_text = models.CharField(max_length=1000)
 
 
