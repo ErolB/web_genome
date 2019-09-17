d859e6a12d22060e10f5ccd64d0575700818f590
ErolB <eroljb@hotmail.com>
2019-09-16 23:04:35 -0400

    removing Heroku-specific items

--- genome/settings.py
+++ genome/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -146,4 +146,4 @@ CELERY_RESULT_SERIALIZER = 'json'
 CELERY_IMPORTS = ("finder.tasks",)
 
 # Heroku setup
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
